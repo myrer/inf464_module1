526e46714ee817174038c4dad10f4c52b3c98bde
Your Name <you@example.com>
2019-11-23 21:12:04 -0500

    next : pick random chromos

--- ga.rb
+++ ga.rb
@@ -1,6 +1,10 @@
+class Individu
+    attr_accessor :chromosome, :score, :probabilite
+end
+
 def chromosome_au_hasard(symbols, n)
   chromosome = Array.new
-  n.times{ chromosome << symbols.sample}
+  n.times{ chromosome << symbols.sample }
   return chromosome
 end
 
@@ -14,31 +18,55 @@ def afficher_chromosome_2d(a, col, rang)
   end
 end
 
+def score(c, ref)
+  somme = 0
+  c.each_with_index do |e,i|
+    if c[i] == ref[i]
+      somme = somme + 1
+    end
+  end
+  return somme
+end
+
 #=====
 #---Hyperparamètres
-nombre_chromosomes = 10
+population = 1000
 colonnes = 13
 rangees = 7
 longueur_chromosome = colonnes*rangees
 symboles = [".", "+"]
-str1d = "...*******....*.........*.*..**...**..**.....*.....*.*.........*...*...**..*.....*......*.."
+
 ref = Array.new
+str1d = "...*******....*.........*.*..**...**..**.....*.....*.*.........*...*...**..*.....*......*.."
 str1d.each_char { |chr| ref << chr  }
 
-#---Population initiale
-chromosomes = Array.new
+#---Générer les individus de la génération initiale
+individus = Array.new
 
-nombre_chromosomes.times {
-  chromosomes << chromosome_au_hasard(symboles, longueur_chromosome)
-}
+population.times do
+  c = chromosome_au_hasard(symboles, longueur_chromosome)
+  s = score(c, ref)
+  individu = Individu.new
+  individu.chromosome =  c
+  individu.score = s
+
+  individus << individu
+end
+
+# Calculer la probabilite de reproduction : score / somme des scores
+somme_scores = 0
+individus.each{|individu| somme_scores = somme_scores + individu.score}
+
+individus.each{|individu| individu.probabilite = individu.score.to_f/somme_scores.to_f}
 
 #Afficher ref
 afficher_chromosome_1d(ref)
 afficher_chromosome_2d(ref,colonnes,rangees)
 puts
 
-chromosomes.each  do |c|
-  afficher_chromosome_1d(c)
-  afficher_chromosome_2d(c,colonnes,rangees)
-  puts
+individus.sort{|a,b| a.probabilite <=> b.probabilite}.each do |individu|
+  #afficher_chromosome_1d(c)
+  puts "#{individu.score} #{individu.probabilite}"
+  #afficher_chromosome_2d(c,colonnes,rangees)
+  #puts
 end
