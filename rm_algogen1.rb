1e1257c3aab0e8420b8444cb8526d912e737a9b9
Your Name <you@example.com>
2019-11-25 21:01:30 -0500

    add rm_algogen1.rb

---
+++ rm_algogen1.rb
@@ -0,0 +1,202 @@
+class Individu
+    attr_accessor :chromosome, :score, :probabilite, :prob_cumulee
+end
+
+def lire_text_art(fname)
+  ary = Array.new
+  f = File.open(fname, "r")
+
+  while ligne = f.gets
+    ligne = ligne.chomp
+    ligne.each_char { |chr| ary << chr  }
+  end
+  f.close
+
+  return ary
+end
+
+def chromosome_au_hasard(symboles, n)
+  chromosome = Array.new
+  n.times{ chromosome << symboles.sample }
+  return chromosome
+end
+
+def afficher_chromosome_1d(c)
+    puts c.join
+end
+
+def afficher_chromosome_2d(a, col, rang)
+  for r in 0..rang-1
+    print "\t"*3
+    puts a[r*col, col].join("")
+  end
+end
+
+def score(c, ref)
+  somme = 0
+  ref.size.times do |i|
+    if ref[i] == c[i]
+      somme = somme + 1
+    end
+  end
+  return somme
+end
+
+def calculer_probabilite_reproduction(individus)
+  somme_scores = 0
+  individus.each{|individu| somme_scores = somme_scores + individu.score}
+  individus.each{|individu| individu.probabilite = individu.score.to_f/somme_scores.to_f}
+  somme_prob = 0.0
+  individus = individus.sort{|a,b| a.probabilite <=> b.probabilite}
+  individus.each do |individu|
+    somme_prob = somme_prob + individu.probabilite
+    individu.prob_cumulee = somme_prob
+  end
+  return individus
+end
+
+def choisir_individu_au_hasard(individus)
+  delta = 0.001
+  while
+    prob = rand
+    tmp = individus.select{|individu| individu.prob_cumulee >= prob - delta and individu.prob_cumulee <= prob + delta}
+    return tmp.first if !tmp.empty?
+  end
+end
+
+def croiser_chromosomes(chromosome1, chromosome2)
+  #Croiser les parents
+  longueur_chromosome = chromosome1.size
+  index1 = rand(longueur_chromosome-1)
+  index2 = rand(longueur_chromosome-1)
+  if index1 > index2
+    tmp = index1
+    index1 = index2
+    index2 = tmp
+  end
+  #puts "#{index1}\t #{index2}"
+  if index1 == 0
+    index1 = 1
+  end
+
+  chromosome_croise = chromosome1[0..(index1-1)] + chromosome2[index1..index2] + chromosome1[(index2+1)..-1]
+  if chromosome_croise.size != longueur_chromosome
+    puts "="*100
+    puts chromosome1.join
+    puts chromosome2.join
+    puts chromosome_croise.join
+    puts "="*100
+    raise "chromosome_croise illegal size"
+  end
+  return chromosome_croise
+end
+
+#=====
+
+#---Hyperparamètres
+population = 5
+nombre_generations = 10
+taux_mutation = 0.02
+nombre_mutations = (taux_mutation * population).to_i
+mutation_par_chromosome = 5
+colonnes = 13
+rangees = 7
+longueur_chromosome = colonnes*rangees
+pcent = 0.0
+
+ref = Array.new
+#str1d  =  ".._\\||\\///.../.........\\..|..0...0...|.|....\\/....|..\\..\\___/../...\\......./.....|......|.."
+ref = lire_text_art("text_art_0.txt")
+symboles = ref.uniq
+max_score = longueur_chromosome*symboles.size
+#afficher_chromosome_1d(ref)
+#puts str1d
+
+#afficher_chromosome_2d(ref, colonnes, rangees)
+
+#---Générer les individus de la génération initiale
+individus = Array.new
+
+
+population.times do
+  c = chromosome_au_hasard(symboles, longueur_chromosome)
+  s = score(c, ref)
+  individu = Individu.new
+  individu.chromosome =  c
+  individu.score = s
+
+  individus << individu
+end
+
+#individus.each {|ind| afficher_chromosome_1d(ind.chromosome)}
+#puts
+#individus.each {|ind| afficher_chromosome_2d(ind.chromosome, colonnes, rangees); puts}
+#exit
+
+#Calculer la probabilite de reproduction : score / somme des scores
+individus = calculer_probabilite_reproduction(individus)
+
+if false
+individus.each_with_index  do |ind, i|
+  puts "Individu #{i}  Score : #{ind.score}"
+  afficher_chromosome_1d(ind.chromosome)
+  afficher_chromosome_2d(ind.chromosome, colonnes, rangees)
+  puts
+end
+exit
+end
+
+best = individus.last
+
+puts "Let's go!"
+nouveaux_individus = Array.new
+nombre_generations.times do |gen|
+  nouveaux_individus.clear
+  population.times do |i|
+
+    parent1 = choisir_individu_au_hasard(individus)
+    parent2 = choisir_individu_au_hasard(individus)
+    chromosome_croise = croiser_chromosomes(parent1.chromosome, parent2.chromosome)
+
+    nouvel_individu = Individu.new
+    nouvel_individu.chromosome =  chromosome_croise
+    nouvel_individu.score = score(chromosome_croise, ref)
+
+    nouveaux_individus << nouvel_individu
+  end
+  nouveaux_individus = calculer_probabilite_reproduction(nouveaux_individus)
+
+  #Mutations
+  if pcent >= 97.0
+    nombre_mutations_ajustees = nombre_mutations/2
+  else
+    nombre_mutations_ajustees = nombre_mutations
+  end
+  nombre_mutations.times do
+    mutation_par_chromosome.times do
+      nouveaux_individus.sample.chromosome[rand(longueur_chromosome)] = symboles.sample
+    end
+  end
+
+  #swap
+  copie = individus
+  individus = nouveaux_individus
+  nouveaux_individus = copie
+
+  best = individus.last
+  max_score = longueur_chromosome
+  pcent = (100*best.score.to_f/max_score.to_f).round
+
+  puts "Gen :  #{gen}: Score : #{pcent}%"
+  afficher_chromosome_2d(best.chromosome,colonnes,rangees)
+
+  if best.score == max_score
+    puts "BINGO!"
+    break
+  end
+end
+
+if false
+puts "\nReference"
+afficher_chromosome_2d(ref,colonnes,rangees)
+end
--- rm_villes3.rb
+++ rm_villes3.rb
@@ -1,36 +1,69 @@
+# La variable villes est un objet Array qui contient 10 éléments qui sont tous
+# des objets String.
+villes =[ "Dallas;USA;22", "Rome;Italie;21", "La Prairie;Canada;2",
+          "New York;USA;10", "Boston;USA;8",
+          "Calgary;Canada;-3","Miami;USA;28","Hong Kong;Chine;24",
+          "Toronto;Canada;5", "Vancouver;Canada;14"]
 
-villes =["Dallas;USA;22", "Rome;Italie;21", "La Prairie;Canada;2", "New York;USA;10", "Boston;USA;8",
-        "Calgary;Canada;-3","Miami;USA;28","Hong Kong;Chine;24", "Toronto;Canada;5", "Vancouver;Canada;14"]
 
-#Agir sur CHAQUE élément avec la méthode .each
-puts "\nToutes les villes\n"
-villes.each do |el|
-	puts el
-end
+puts "\nAfficher toutes les villes\n"
+# La méthode .each permet d'agir sur CHAQUE élément d'un objet Array.
+# La variable el prendra la valeur de CHAQUE objet String contenu dans villes.
+# Notez que la variable el est encadré par des lignes verticales | |.
+# La variable el changera de valeur à chaque tour de boucle.
+# Toutes les commandes encadrés par do et end seront exécutées pour chaque
+# élément de l'objet Array villes.
+# La variable el existe seulement dans le bloc de code fourni.
+villes.each do |el|   # La variable el existe ici.
+	puts el             # La variable el existe ici aussi.
+end                   # La variable el n'exsite plus à partir de cette ligne.
 
-puts "\nVilles canadiennes\n" 
+puts "\nAfficher les villes canadiennes\n"
+# La méthode .select SÉLECTIONNE des éléments d'un objet Array selon les
+# directives programmées dans le bloc de code fourni : {|el| el.include?("Canada")}
+# La variable el prendra la valeur de CHAQUE objet String contenu dans villes.
+# Si la valeur de la variable el inclut un objet String "Canada", la valeur de
+# el sera SÉLECTIONNÉE et ajoutée à l'objet Array villes_candiennes.
+# La variable el existe seulement dans le bloc de code fourni.
 villes_canadiennes = villes.select {|el| el.include?("Canada")}
 villes_canadiennes.each {|el| puts el}
 
+# La méthode .sort permet de TRIER les éléments d'un objet Array.
+# Le bloc de code encadré par do et end précise comment le tri sera effectué.
+# Les variables a et b prendront les valeurs des éléments de l'objet Array au
+# cours de l'algorithm de triage.
+# Notez que la variable a et b sont encadrés par des lignes verticales | |.
+# Ici, on souhaite trier les villes par ordre croissant de température.
+# Pour accomplir cela, il faut extraire l'information de l'objet String et
+# convertir en nombre (Fixnum) avec la méthode .to_i .
+# La méthode .split sépare un objet String en plusieurs éléments. La séparation
+# est effectuée selon un autre objet String fourni (";" dans ce cas).
+# La méthode .split place les éléments séparés dans un objet Array.
+# L'information de la température est située dans la position 2 de l'objet Array
+# donnees.
+# Le symbole <=> compare deux expressions en Ruby.
 villes_par_temp= villes.sort do |a,b|
-	donnees = a.split(";")
-	temp_a = donnees[2].to_i
-	
+	donnees = a.split(";")   # donnees est un objet Array qui contient 3 éléments
+	temp_a = donnees[2].to_i # .to_i convertit un objet String en un objet Fixnum
+
 	donnees = b.split(";")
 	temp_b = donnees[2].to_i
-	
-	temp_a <=> temp_b
-end
 
-puts "\nVilles en ordre de température\n"
+	temp_a <=> temp_b  # Comparons les températures pour les trier
+end #Les variables a, b, donnees, temp_a et temp_b n'exsitent plus ...
+
+puts "\nAfficher les villes en ordre de température\n"
 villes_par_temp.each {|el| puts el}
 
-puts "\n3 plus froides\n"
-villes_par_temp[0,3].each{|el| puts el}
+puts "\nAfficher les 3 villes les plus froides\n"
+villes_par_temp[0..2].each{|el| puts el}
 
-puts "\n3 plus chaudes"
+puts "\nAfficher les 3 villes les plus chaudes"
 villes_par_temp.reverse[0..2].each {|el| puts el}
+# Voici une autre façon de procéder
+villes_par_temp.[-3..-1].each {|el| puts el} #Cette façon est plus rapide!
 
+puts "\nAfficher la moyenne de la température"
 somme = 0
 villes.each { |el|
 	donnees = el.split(";")
@@ -38,10 +71,5 @@ villes.each { |el|
 	somme = somme + temp
 }
 
-moy = somme.to_f/villes.size
+moy = somme.to_f/villes.size  #Pourquoi .to_f?
 puts "\nLa moyenne de la temp est :#{moy} C."
-
-usa ,others = villes.partition{|el| el.include?("USA")}
-puts usa
-puts
-puts others
